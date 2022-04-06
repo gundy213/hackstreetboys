456e84f8908adfc99058ccce3bdc4f9db9174272
zac2203 <99301695+zac2203@users.noreply.github.com>
2022-04-05 20:28:14 -0500

    Update functions.js

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -10,22 +10,3 @@ document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("lastUpdated").innerHTML = updated;
 
 })
-
-const btn = document.querySelector(".btn-toggle");
-// Select the stylesheet <link>
-const theme = document.querySelector("#theme-link");
-
-// Listen for a click on the button
-btn.addEventListener("click", function() {
-   alert("Button works");
-  // If the current URL contains "css/lightTheme.css"
-  if (theme.getAttribute("href") == "css/lightTheme.css") {
-    // ... then switch it to "css/darkTheme.css"
-    theme.href = "css/darkTheme.css";
-  // Otherwise...
-  } 
-  else {
-    // ... switch it to "css/lightTheme.css"
-    theme.href = "css/lightTheme.css";
-  }
-})
