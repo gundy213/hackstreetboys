4a8775dd369a0f38ed795b7296f00eeac2dc624e
gundy213 <gundy213@yahoo.com>
2022-04-17 17:18:42 -0500

    ye

--- JavaScript/formCheck.js
+++ JavaScript/formCheck.js
@@ -7,33 +7,33 @@ $(document).ready(function() {
         const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
         const email = $("#email").val().trim();
         if (email == "" || email == "null") { 
-            $("#email").next().text("required");
+            $("#emailRequired").text("* REQUIRED");
             e.preventDefault();
         } else if ( !emailPattern.test(email) ) {
-            $("#email").next().text("Must be a valid email address.");
+            $("#emailRequired").next().text("Must be a valid email address.");
             e.preventDefault();
         } else {
-            $("#email").next().text("");
+            $("#emailRequired").text("");
         }
         $("#email").val(email);
         
         // validate the first name entry 
         const firstName = $("#fname").val().trim(); 
         if (firstName == "" || firstName == "null") {
-            $("#fname").next().text("First name is required.");
+            $("#fnameRequired").text("* REQUIRED");
             e.preventDefault();
         } else {
-            $("#fname").next().text("");
+            $("#fnameRequired").text("");
         }
         $("#fname").val(firstName);
 
         // validate the last name entry 
         const lastName = $("#lname").val().trim(); 
         if (lastName == "" || lastName == "null") {
-            $("#lname").next().text("Last name is required.");
+            $("#lnameRequired").text("* REQUIRED");
             e.preventDefault();
         } else {
-            $("#lname").next.text("");
+            $("#lnameRequired").text("");
         }
         $("#lname").val(lastName);
     });
